1782ef6a7a2dddc457cd2cf45489cb857d78ee0f
Amr Salem <32042806+amrsa1@users.noreply.github.com>
2023-03-04 20:18:47 +0100

    Create start_emu_headless.sh

---
+++ start_emu_headless.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+BL='\033[0;34m'
+G='\033[0;32m'
+RED='\033[0;31m'
+YE='\033[1;33m'
+NC='\033[0m' # No Color
+
+emulator_name=${EMULATOR_NAME}
+
+function check_hardware_acceleration() {
+    HW_ACCEL_SUPPORT=$(grep -E -c '(vmx|svm)' /proc/cpuinfo)
+    if [[ $HW_ACCEL_SUPPORT == 0 ]]; then
+        echo "-accel off"
+    else
+        echo "-accel on"
+    fi
+}
+
+hw_accel_flag=$(check_hardware_acceleration)
+
+function launch_emulator () {
+  adb devices | grep emulator | cut -f1 | xargs -I {} adb -s "{}" emu kill
+  options="-avd ${emulator_name} -no-window -no-snapshot-load -noaudio -no-boot-anim -memory 2048 ${hw_accel_flag}"
+  echo "emulator "$options
+  if [[ "$OSTYPE" == *linux* ]]; then
+    nohup emulator $options -gpu off &
+  elif [ "$OSTYPE" == *darwin* ] || [ "$OSTYPE" == *macos* ]; then
+    nohup emulator $options -gpu swiftshader_indirect &
+  fi
+
+  if [ $? -ne 0 ]; then
+    echo "Error launching emulator"
+    return 1
+  fi
+};
+
+function check_emulator_status () {
+  printf "${G}==> ${BL}Checking emulator booting up status 🧐${NC}\n"
+  start_time=$(date +%s)
+  spinner=( "⠹" "⠺" "⠼" "⠶" "⠦" "⠧" "⠇" "⠏" )
+  i=0
+  # Get the timeout value from the environment variable or use the default value of 240 seconds (4 minutes)
+  timeout=${EMULATOR_TIMEOUT:-240}
+
+  while true; do
+    result=$(adb shell getprop sys.boot_completed 2>&1)
+
+    if [ "$result" == "1" ]; then
+      printf "\e[K${G}==> \u2713 Emulator is ready : '$result'           ${NC}\n"
+      adb devices -l
+      adb shell input keyevent 82
+      break
+    elif [ "$result" == "" ]; then
+      printf "${YE}==> Emulator is partially Booted! 😕 ${spinner[$i]} ${NC}\r"
+    else
+      printf "${RED}==> $result, please wait ${spinner[$i]} ${NC}\r"
+      i=$(( (i+1) % 8 ))
+    fi
+
+    current_time=$(date +%s)
+    elapsed_time=$((current_time - start_time))
+    if [ $elapsed_time -gt $timeout ]; then
+      printf "${RED}==> Timeout after ${timeout} seconds elapsed 🕛.. ${NC}\n"
+      break
+    fi
+    sleep 0.2
+  done
+};
+
+
+function disable_animation() {
+  adb shell "settings put global window_animation_scale 0.0"
+  adb shell "settings put global transition_animation_scale 0.0"
+  adb shell "settings put global animator_duration_scale 0.0"
+};
+
+function hidden_policy() {
+  adb shell "settings put global hidden_api_policy_pre_p_apps 1;settings put global hidden_api_policy_p_apps 1;settings put global hidden_api_policy 1"
+};
+
+launch_emulator
+sleep 2
+check_emulator_status
+sleep 1
+disable_animation
+sleep 1
+hidden_policy
+sleep 1
